81ccf5347041c51903933514ec72a325ae51022b
Veronica <dugancathal@gmail.com>
2015-08-05 12:26:23 -0600

    As a user, I can see all pings

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
 require 'json'
 
-get '/ping' do
+$pings = []
+
+post '/pings' do
+  $pings << {created_at: Time.now}
   {ping: true}.to_json
 end
 
+get '/pings' do
+  $pings.to_json
+end
